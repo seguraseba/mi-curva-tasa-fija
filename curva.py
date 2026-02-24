bf62ea45da7caf52befb5073593f2c3cde6ccae8
Sebastián Segura <ssegura@balanz.com>
2026-02-24 11:51:14 -0300

    Arreglo hovertemplate plotly

--- curva.py
+++ curva.py
@@ -1083,7 +1083,7 @@ else:
 # =========================
 # FILA 2: CER (cupón cero)
 # =========================
-st.markdown("## CER (cupón cero)")
+st.markdown("## CER)")
 
 if df_cer is None or df_cer.empty:
     st.info("No se encontraron instrumentos CER.")
